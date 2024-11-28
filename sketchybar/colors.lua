8bb84ed542dbdb6cca2581babec542cf1bac4383
hrtowii <68852354+hrtowii@users.noreply.github.com>
2024-11-28 13:22:12 +0800

    tokyonight

--- sketchybar/colors.lua
+++ sketchybar/colors.lua
@@ -1,30 +1,32 @@
 return {
-	black = 0xff1d2021,
-	white = 0xffebdbb2,
-	red = 0xfffb4934,
-	green = 0xffb8bb26,
-	blue = 0xff83a598,
-	yellow = 0xfffabd2f,
-	orange = 0xfffe8019,
-	magenta = 0xffd3869b,
-	grey = 0xffa89984,
+	bg = "#24283b",
+	bg_dark = "#1f2335",
+	black = 0x1f2335,
+	white = 0xffc0caf5,
+	red = 0xfff7768e,
+	green = 0xff9ece6a,
+	blue = 0xff7aa2f7,
+	yellow = 0xffe0af68,
+	orange = 0xffff9e64,
+	magenta = 0xffbb9af7,
+	grey = 0xff565f89,
 	transparent = 0x00000000,
-
+  
 	bar = {
-		bg = 0xff1d2021,
-		border = 0xffebdbb2,
+	  bg = 0xff1f2335,
+	  border = 0xffa9b1d6,
 	},
 	popup = {
-		bg = 0xc01f2335,
-		border = 0xff7f8490,
+	  bg = 0xc01f2335,
+	  border = 0xff7dcfff,
 	},
-	bg1 = 0xff3c3836,
-	bg2 = 0xff32302f,
-
+	bg1 = 0xff292e42,
+	bg2 = 0xff1f2335,
+  
 	with_alpha = function(color, alpha)
-		if alpha > 1.0 or alpha < 0.0 then
-			return color
-		end
-		return (color & 0x00ffffff) | (math.floor(alpha * 255.0) << 24)
+	  if alpha > 1.0 or alpha < 0.0 then
+		return color
+	  end
+	  return (color & 0x00ffffff) | (math.floor(alpha * 255.0) << 24)
 	end,
-}
+  }
