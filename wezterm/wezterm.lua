72f34dca1e36086894f4cbeadb7789fe1d188efb
hrtowii <68852354+hrtowii@users.noreply.github.com>
2024-11-28 13:30:43 +0800

    more tokyonight

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -7,9 +7,10 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
-config.color_scheme = "Gruvbox dark, hard (base16)"
+config.color_scheme = "Tokyo Night"
 config.window_close_confirmation = "NeverPrompt"
 config.window_decorations = "RESIZE"
 config.font = wezterm.font("JetBrains Mono")
+config.hide_tab_bar_if_only_one_tab = true
 -- and finally, return the configuration to wezterm
 return config
