49e44925b1b106cb2347fabf339dd563436858e0
htrowii <leonghongkit@gmail.com>
2025-06-16 03:44:04 +0800

    meow

--- nixos/home-manager/modules/keyring.nix
+++
@@ -1,6 +0,0 @@
-{pkgs, ...}: {
-services.gnome.gnome-keyring.enable = true;
-environment.systemPackages = [pkgs.libsecret]; # google-chrome works with gnome-keyring through this
-environment.variables.XDG_RUNTIME_DIR = "/run/user/$UID"; # this is what was missing
-security.pam.services.ly.enableGnomeKeyring = true; # ly or any other display manager (sddm/greetd/gdm...)
-}
---
+++ nixos/modules/keyring.nix
@@ -0,0 +1,34 @@
+{ pkgs, config, lib, ... }:
+# https://nixos.wiki/wiki/GNOME#Running_GNOME_programs_outside_of_GNOME
+with lib;
+let cfg = config.jd.gnome;
+in {
+  options.jd.gnome = {
+    enable = mkOption {
+      description = "Enable GNOME programs";
+      type = types.bool;
+      default = false;
+    };
+
+    keyring = {
+      enable = mkOption {
+        description = "Enable gnome-keyring";
+        type = types.bool;
+        default = false;
+      };
+    };
+  };
+
+  config = mkIf cfg.enable (mkMerge [
+    {
+      programs.dconf.enable = true;
+      # Fixes the org.a11y.Bus not provided by .service file error
+      services.gnome.at-spi2-core.enable = true;
+    }
+    (mkIf cfg.keyring.enable {
+      environment.systemPackages = [ pkgs.libsecret ];
+      services.gnome.gnome-keyring.enable = true;
+      services.dbus.packages = [ pkgs.seahorse ];
+    })
+  ]);
+}
