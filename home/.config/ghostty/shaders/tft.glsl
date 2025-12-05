aba9e7986b2cbe99cb456aa9d39fd339564d4d44
hrtowii <68852354+hrtowii@users.noreply.github.com>
2025-12-06 01:47:12 +0800

    ghostty

---
+++ home/.config/ghostty/shaders/tft.glsl
@@ -0,0 +1,24 @@
+/** Size of TFT "pixels" */
+float resolution = 4.0;
+
+/** Strength of effect */
+float strength = 0.3;
+
+void _scanline(inout vec3 color, vec2 uv)
+{
+    float scanline = step(1.2, mod(uv.y * iResolution.y, resolution));
+    float grille   = step(1.2, mod(uv.x * iResolution.x, resolution));
+    float factor = mix(1.0, max(1.0 - strength, scanline * grille), 1.0);
+    color *= factor;
+}
+
+void mainImage(out vec4 fragColor, in vec2 fragCoord)
+{
+    vec2 uv = fragCoord.xy / iResolution.xy;
+    vec3 color = texture(iChannel0, uv).rgb;
+
+    _scanline(color, uv);
+
+    fragColor.xyz = color;
+    fragColor.w   = 1.0;
+}
