ffb0845909184bb504afd7d3495447316d11d515
htrowii <leonghongkit@gmail.com>
2026-02-09 19:23:33 +0800

    fix

--- nixos/home-manager/scripts/toggle-refresh.sh
+++ nixos/home-manager/scripts/toggle-refresh.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 MONITOR="eDP-2"
-CURRENT=$(hyprctl monitors | grep -A 15 "$MONITOR" | grep "refresh rate" | awk '{print $3}')
+CURRENT=$(hyprctl monitors | grep "$MONITOR" -A 1 | grep "@" | grep -oP '\d+(?=\.' | head -1)
 
-if [ "$CURRENT" = "144.00" ]; then
+if [ "$CURRENT" = "144" ]; then
     hyprctl keyword "monitor $MONITOR,1920x1200@60,0x0,1.5"
     notify-send "Refresh Rate" "Set to 60Hz (Battery Saver)"
 else
