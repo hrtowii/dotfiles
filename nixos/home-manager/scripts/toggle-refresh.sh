7c5284fa0ecedc49b80f9a8f4e578c63b605648b
htrowii <leonghongkit@gmail.com>
2026-02-10 16:14:28 +0800

    sdr

--- nixos/home-manager/scripts/toggle-refresh.sh
+++ nixos/home-manager/scripts/toggle-refresh.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 MONITOR="eDP-2"
-CURRENT=$(hyprctl monitors | grep "$MONITOR" -A 1 | grep "@" | grep -oP '\d+(?=\.' | head -1)
+CURRENT=$(hyprctl monitors | grep "$MONITOR" -A 1 | grep "@" | sed 's/.*@\([0-9]*\)\..*/\1/')
 
 if [ "$CURRENT" = "144" ]; then
     hyprctl keyword "monitor $MONITOR,1920x1200@60,0x0,1.5"
---
+++ nixos/modules/sdr.nix
@@ -0,0 +1,13 @@
+{ config, pkgs, ... }:
+{
+  boot.kernelParams = [ "modprobe.blacklist=dvb_usb_rtl28xxu" ]; # blacklist this module
+  hardware.rtl-sdr.enable = true;
+  users.users.venti.extraGroups = [ "plugdev" ];
+  environment.systemPackages = with pkgs; [
+    wget
+    vim
+    libusb
+    rtl-sdr
+    gqrx
+  ];
+}
