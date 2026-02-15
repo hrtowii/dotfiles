e1756e5235f18e1923673a040e631b4ba90612af
htrowii <leonghongkit@gmail.com>
2026-02-16 03:36:24 +0800

    mouse specific sens

--- nixos/modules/python.nix
+++ nixos/modules/python.nix
@@ -1,25 +1,25 @@
 {pkgs, ...}: {
   environment.systemPackages = with pkgs; [
-    (python311.withPackages (ps:
-      with ps; [
-        # i3ipc
-        requests
-        ipython
-        # six
-        psutil
-        # pynvml
-        # pyqtgraph
-        # pyqt6
-        # pyyaml
-        pillow
-        # jedi
-        # libcst
-        wheel
-        jupyterlab
-        # datasets
-        # debugpy
-      ]))
-    isort
+    # (python311.withPackages (ps:
+    #   with ps; [
+    #     # i3ipc
+    #     requests
+    #     ipython
+    #     # six
+    #     psutil
+    #     # pynvml
+    #     # pyqtgraph
+    #     # pyqt6
+    #     # pyyaml
+    #     pillow
+    #     # jedi
+    #     # libcst
+    #     wheel
+    #     jupyterlab
+    #     # datasets
+    #     # debugpy
+    #   ]))
+    # isort
     uv
     python311
     ruff
