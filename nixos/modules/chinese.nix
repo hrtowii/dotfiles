f5455a65a34cfa031d52c61fef0f310b6b2886f3
htrowii <leonghongkit@gmail.com>
2026-02-17 00:54:35 +0800

    chinese

--- nixos/modules/chinese.nix
+++ nixos/modules/chinese.nix
@@ -4,14 +4,13 @@
     enable = true;
     type = "fcitx5";
     fcitx5 = {
-      plasma6Support = true;
       waylandFrontend = true;
       ignoreUserConfig = true;
       addons = with pkgs; [
         fcitx5-mozc
         fcitx5-gtk
         fcitx5-pinyin-zhwiki
-        fcitx5-chinese-addons
+        qt6Packages.fcitx5-chinese-addons
       ];
       settings = {
         inputMethod = {
