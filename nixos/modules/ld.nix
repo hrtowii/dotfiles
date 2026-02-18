648b9216d78b376de4ce3462be6b1ae34e0c0903
htrowii <leonghongkit@gmail.com>
2026-02-18 11:48:33 +0800

    nix

--- nixos/modules/ld.nix
+++ nixos/modules/ld.nix
@@ -5,5 +5,28 @@
     enable = true;
   };
 
+programs.nix-ld.libraries = with pkgs; [
+  libglvnd
+  mesa
+
+  xorg.libX11
+  xorg.libXext
+  xorg.libXrender
+  xorg.libXrandr
+  xorg.libxcb
+  xorg.libXi
+  xorg.libXcursor
+  xorg.libXinerama
+
+  libxkbcommon
+
+  zlib
+  glib
+  fontconfig
+  freetype
+  dbus
+
+  stdenv.cc.cc
+];
 }
 
