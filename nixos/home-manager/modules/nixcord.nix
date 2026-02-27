f06c0250882fb95c0ad1a6bb93fcd679d962793e
htrowii <leonghongkit@gmail.com>
2026-02-27 09:15:57 +0800

    nixcord

--- nixos/home-manager/modules/nixcord.nix
+++ nixos/home-manager/modules/nixcord.nix
@@ -2,4 +2,13 @@
 {
   imports = [ inputs.nixcord.homeModules.nixcord ];
   # ... config
+  programs.nixcord = {
+    enable = true;
+
+    discord = {
+      enable = true;
+      branch = "canary";
+      autoscroll.enable = true;
+    };
+  };
 }
