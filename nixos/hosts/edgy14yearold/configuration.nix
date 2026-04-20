b0d27ac600abe509c7c9a6f542856c1bb0613cdb
Your Name <you@example.com>
2026-04-21 02:07:43 +0800

    separate

--- nixos/hosts/edgy14yearold/configuration.nix
+++ nixos/hosts/edgy14yearold/configuration.nix
@@ -9,7 +9,7 @@
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
       ./system-packages.nix
-      ../../modules/thinkpad
+      ../../modules/edgy14yearold
     ];
 
   boot.loader.systemd-boot.enable = true;
