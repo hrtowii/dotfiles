efeed824a66913ac79ca3b6f0f7de9f97b08e4d1
Your Name <you@example.com>
2026-07-05 01:34:55 +0800

    fix if not working

--- nixos/home-manager/modules/spicetify.nix
+++ nixos/home-manager/modules/spicetify.nix
@@ -1,40 +1,47 @@
-{ config, inputs, lib, pkgs, ... }:
+{ config, inputs, lib, pkgs, options, ... }:
 let
   spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.stdenv.hostPlatform.system};
+  
+  stylixImported = options ? stylix;
+  
   stylixColors = lib.attrByPath [ "lib" "stylix" "colors" ] null config;
-  stylixEnabled = lib.attrByPath [ "stylix" "enable" ] false config;
+  stylixEnabled = stylixImported && (lib.attrByPath [ "stylix" "enable" ] false config);
 in
-{
-  imports = [
-    inputs.spicetify-nix.homeManagerModules.default
-  ];
-
-  stylix.targets.spicetify.enable = false;
-
-  programs.spicetify = {
-    enable = true;
-
-    enabledExtensions = with spicePkgs.extensions; [
-      # adblock
-      hidePodcasts
-      shuffle
+lib.mkMerge [
+  {
+    imports = [
+      inputs.spicetify-nix.homeManagerModules.default
     ];
 
-    theme = spicePkgs.themes.text;
-    customColorScheme = lib.mkIf (stylixColors != null && stylixEnabled) (with stylixColors.withHashtag; {
-      accent             = base0D;
-      accent-active      = base0C;
-      accent-inactive    = base03;
-      banner             = base0D;
-      border-active      = base0C;
-      border-inactive    = base03;
-      header             = base04;
-      highlight          = base02;
-      main               = base00;
-      notification       = base0D;
-      notification-error = base08;
-      subtext            = base04;
-      text               = base05;
-    });
-  };
-}
+    programs.spicetify = {
+      enable = true;
+
+      enabledExtensions = with spicePkgs.extensions; [
+        # adblock
+        hidePodcasts
+        shuffle
+      ];
+
+      theme = spicePkgs.themes.text;
+      customColorScheme = lib.mkIf (stylixColors != null && stylixEnabled) (with stylixColors.withHashtag; {
+        accent             = base0D;
+        accent-active      = base0C;
+        accent-inactive    = base03;
+        banner             = base0D;
+        border-active      = base0C;
+        border-inactive    = base03;
+        header             = base04;
+        highlight          = base02;
+        main               = base00;
+        notification       = base0D;
+        notification-error = base08;
+        subtext            = base04;
+        text               = base05;
+      });
+    };
+  }
+
+  (lib.optionalAttrs stylixEnabled {
+    stylix.targets.spicetify.enable = false;
+  })
+]
--- nixos/home-manager/modules/swaync.nix
+++ nixos/home-manager/modules/swaync.nix
@@ -1,7 +1,9 @@
-{ config, lib, ... }:
+{ config, options, lib, ... }:
 let
+  stylixImported = options ? stylix;
+  
   stylixColors = lib.attrByPath [ "lib" "stylix" "colors" ] null config;
-  stylixEnabled = lib.attrByPath [ "stylix" "enable" ] false config;
+  stylixEnabled = stylixImported && (lib.attrByPath [ "stylix" "enable" ] false config);
 
   colorDefs = if stylixColors != null && stylixEnabled then
     with stylixColors.withHashtag; ''
@@ -27,43 +29,48 @@ let
     '';
 
   customCss = builtins.readFile ../config/swaync/style.css;
-in {
-  stylix.targets.swaync.enable = false;
+in
+lib.mkMerge [
+  {
+    services.swaync = {
+      enable = true;
 
-  services.swaync = {
-    enable = true;
+      settings = {
+        positionX = "right";
+        positionY = "top";
+        layer = "overlay";
+        control-center-layer = "top";
+        layer-shell = true;
+        cssPriority = "application";
+        control-center-margin-top = 0;
+        control-center-margin-bottom = 0;
+        control-center-margin-right = 0;
+        control-center-margin-left = 0;
+        notification-2fa-action = true;
+        notification-inline-replies = false;
+        notification-icon-size = 64;
+        notification-body-image-height = 100;
+        notification-body-image-width = 200;
+        timeout = 10;
+        timeout-low = 5;
+        timeout-critical = 0;
+        fit-to-screen = true;
+        control-center-width = 500;
+        control-center-height = 600;
+        notification-window-width = 500;
+        keyboard-shortcuts = true;
+        image-visibility = "when-available";
+        transition-time = 200;
+        hide-on-clear = false;
+        hide-on-action = true;
+        script-fail-notify = false;
+      };
 
-    settings = {
-      positionX = "right";
-      positionY = "top";
-      layer = "overlay";
-      control-center-layer = "top";
-      layer-shell = true;
-      cssPriority = "application";
-      control-center-margin-top = 0;
-      control-center-margin-bottom = 0;
-      control-center-margin-right = 0;
-      control-center-margin-left = 0;
-      notification-2fa-action = true;
-      notification-inline-replies = false;
-      notification-icon-size = 64;
-      notification-body-image-height = 100;
-      notification-body-image-width = 200;
-      timeout = 10;
-      timeout-low = 5;
-      timeout-critical = 0;
-      fit-to-screen = true;
-      control-center-width = 500;
-      control-center-height = 600;
-      notification-window-width = 500;
-      keyboard-shortcuts = true;
-      image-visibility = "when-available";
-      transition-time = 200;
-      hide-on-clear = false;
-      hide-on-action = true;
-      script-fail-notify = false;
+      style = colorDefs + customCss;
     };
+  }
 
-    style = colorDefs + customCss;
-  };
-}
+  (lib.optionalAttrs stylixEnabled {
+    stylix.targets.swaync.enable = false;
+  })
+]
